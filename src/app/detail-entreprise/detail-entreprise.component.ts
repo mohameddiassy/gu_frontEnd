4e5e65d2254879a0b06c4fd33389b8eafe17a2b3
mohameddiassy <Diassmohamed97@gmail.com>
2021-09-19 12:32:17 +0000

    liste des agents  pour une entreprise donnee

--- src/app/detail-entreprise/detail-entreprise.component.ts
+++ src/app/detail-entreprise/detail-entreprise.component.ts
@@ -10,14 +10,18 @@ import { DataService } from '../data.service';
 export class DetailEntrepriseComponent implements OnInit {
   id_entreprise:number=0
   entreprise:any={}
+ 
   constructor(private router:ActivatedRoute,public data:DataService) { }
 
   ngOnInit(): void {
+  
     this.router.params.subscribe((params:any)=>{
       this.id_entreprise=params["id_entreprise"];
       if (this.id_entreprise) {
         console.log("id_entreprise est present")
         this.recevoir_entreprise()
+        this.recevoir_agent()
+        
       } else {
         console.log("id_entreprise est absent")
         
@@ -27,6 +31,11 @@ export class DetailEntrepriseComponent implements OnInit {
   recevoir_entreprise(){
     this.data.requete_post("get_entreprise_by_id.php",{id_entreprise:this.id_entreprise},(data:any)=>{
       this.entreprise=data[0]
+    })
+  }
+  recevoir_agent(){
+    this.data.requete_post("get_enregistreur_by_entreprise.php",{id_entreprise:this.id_entreprise},(data:any)=>{
+      this.data.les_agents=data
     })
   }
 
