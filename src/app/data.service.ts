8b6241ef41fa22fc70170d4c9179da79118c473c
Amarmouhamed <amarmouhamed4@gmail.com>
2021-09-19 00:55:55 +0000

    0055

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,6 +8,7 @@ export class DataService {
   url="http://192.168.1.8/gestionuniversel_back/"
   les_produits:any[]=[]
   les_sorties:any[]=[]
+  utilisateur_connecte:any
   constructor(private http:HttpClient) { }
   // requete_post("inscription.php",{prenom:"mouhamed",nom:"Amar"},(data:any)=>{//apres reception})
   requete_post(page:string,parametres:any,calback:Function){
