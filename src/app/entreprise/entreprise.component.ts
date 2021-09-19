4c4b35c0352e6d7180de0e8f51fe64dc0bc1946b
mohameddiassy <Diassmohamed97@gmail.com>
2021-09-19 00:10:46 +0000

    recuperation de la liste des entreprises

--- src/app/entreprise/entreprise.component.ts
+++ src/app/entreprise/entreprise.component.ts
@@ -6,15 +6,11 @@ import { DataService } from '../data.service';
   styleUrls: ['./entreprise.component.css']
 })
 export class EntrepriseComponent implements OnInit {
-infos_form:any={}
-  constructor(public data:DataService) { }
-
+  listeEntreprise : any;
+  constructor(private entrepriseService : DataService) { }
+  
   ngOnInit(): void {
-  }
-  inscrire(){
-    console.log("les informations du formulaire sont: ",this.infos_form)
-    this.data.requete_post("info_entreprise.php",{utilisateur:JSON.stringify(this.infos_form)},(data:any)=>{
-
-    })
+  
   }
 }
+
