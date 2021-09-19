53e10ebed2d76cc1a4dcc195051e625134198064
Amarmouhamed <amarmouhamed4@gmail.com>
2021-09-19 12:31:08 +0000

    1230

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -11,7 +11,7 @@ export class FooterComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  message_texte(un_logo:any){
-    return "https://api.whatsapp.com/send?text=Salut!😍 J'ai obtenu de *jolies montages photos* avec ce site en quelques secondes et *gratuitement*. Rendez vous vite sur *Amar Montage Logo* en cliquant sur ce lien 👉 https://logo.groupemeta.com/"+un_logo.id_logo+" .Ceci est une production de l'entreprise *META* (plus d'informations 👉  contact@groupemeta.com site officiel👉 https://groupemeta.com). Merci de partager l'information avec vos proches😍";
+  message_texte(){
+    return "https://api.whatsapp.com/send?phone=+221778637918";
   }
 }
