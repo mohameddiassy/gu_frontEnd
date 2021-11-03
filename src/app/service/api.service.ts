27631cfc88f20bdb456f1f467110fa694c2d3650
mohameddiassy <Diassmohamed97@gmail.com>
2021-11-03 00:34:04 +0000

    commite 200

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -8,8 +8,8 @@ import { Observable, Subject } from 'rxjs';
 export class ApiService {
 
   // url="https://gu.groupemeta.com/nouveau/api.php"
-   url="http://192.168.1.13/gestionuniversel_back/nouveau/api.php"
-  // url="http://localhost/gestionuniversel_back/nouveau/api.php"
+  // url="http://192.168.1.106/gestionuniversel_back/nouveau/api.php"
+   url="http://localhost/gestionuniversel_back/nouveau/api.php"
   global:any={
     fenetres:[],
     sidenavbool:false,
@@ -95,7 +95,7 @@ export class ApiService {
       this.bool[cle]=false;
     }
   }
-  
+
   parse(quantite:any)
   {
     if(parseInt(quantite).toString()=='NaN'){
@@ -103,6 +103,6 @@ export class ApiService {
     }else{
       return parseInt(quantite)
     }
-    
+
   }
 }
