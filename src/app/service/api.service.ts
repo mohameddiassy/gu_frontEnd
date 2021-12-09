9f2d2b353407d1c05b18f2b7f4d10ff4d32197e4
Amarmouhamed <amarmouhamed4@gmail.com>
2021-12-09 00:01:48 +0000

    0001

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -10,7 +10,7 @@ export class ApiService {
   // url="https://gu.h24code.com/nouveau/api.php"
   // url="http://192.168.1.19/gestionuniversel_back/nouveau/api.php"
   url="http://192.168.1.13/gestionuniversel_back/nouveau/api.php"
-  //url="http://localhost/gestionuniversel_back/nouveau/api.php"
+  // url="http://localhost/gestionuniversel_back/nouveau/api.php"
 
   global:any={
     fenetres:[],
