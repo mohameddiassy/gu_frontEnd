5865c21948a50baede20bea9c3a694fd445a27b9
mohameddiassy <Diassmohamed97@gmail.com>
2021-12-12 05:03:36 +0000

    ll

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -9,7 +9,7 @@ export class ApiService {
 
   // url="https://gu.h24code.com/nouveau/api.php"
   // url="http://192.168.1.19/gestionuniversel_back/nouveau/api.php"
-   url="http://192.168.1.21/gestionuniversel_back/nouveau/api.php"
+   url="http://192.168.1.5/gestionuniversel_back/nouveau/api.php"
  // url="http://localhost/gestionuniversel_back/nouveau/api.php"
 
   global:any={
